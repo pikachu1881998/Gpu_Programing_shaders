a5a0ec9862ce79e3457636a3fdc4108a3a1eb076
kdesai63 <kdesai63@gatech.edu>
2022-01-25 13:21:14 -0500

    initial

---
+++ HW3_problem3/Assets/Scripts/SetupTextureProjection.cs
@@ -0,0 +1,27 @@
+﻿// Aaron Lanterman, June 19, 2014; updated June 15, 2019
+
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class SetupTextureProjection : MonoBehaviour {
+	
+	private Matrix4x4 projectorMatrix;
+	private Matrix4x4 offsetMatrix;
+	private Camera myCamera;
+
+	void Start() {
+		Vector3 halfs = 0.5f * Vector3.one;
+		offsetMatrix = Matrix4x4.TRS(halfs, Quaternion.identity, halfs);
+		myCamera = GetComponent<Camera> ();
+	}
+	
+	void Update() {
+		projectorMatrix =  offsetMatrix * myCamera.projectionMatrix * myCamera.worldToCameraMatrix;
+		// Quick & dirty; it would generally be better to set properties in specific materials
+		Shader.SetGlobalMatrix("_myProjectorMatrixVP", projectorMatrix);   
+		// Debug.Log (projectorMatrix); 
+		Shader.SetGlobalVector("_spotlightDir", transform.forward);
+	}
+}
